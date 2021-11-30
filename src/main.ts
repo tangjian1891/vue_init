ef67f22ae732a2b0d0b9439369a499021ac072a3
tangjian1891 <tangjian1891@163.com>
2021-11-30 14:04:56 +0800

    新增 环境变量与对应配置文件

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,11 @@ import { createPinia } from "pinia";
 const app = createApp(App);
 createLoading();
 createDialog();
+console.warn("当前所属模式",import.meta.env.VITE_APP_MODE); //自定义的三种 dev uat prod
+console.log(import.meta.env.BASE_URL);//路径
+// console.log(import.meta.env.DEV);//是否为开发 Boolean
+// console.log(import.meta.env.MODE);//MODE就是当前注入的。与  VITE_APP_MODE 相同
+// console.log(import.meta.env.PROD);//是否为打包 Boolean
 app.use(router);
 app.use(createPinia())
 app.mount("#app");
